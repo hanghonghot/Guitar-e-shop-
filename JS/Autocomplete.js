02cda2ea75a82be579080e35d57e711f13e0c15a
hanghonghot <hanghonghot6@gmail.com>
2023-10-19 01:33:42 +0200

    Create Autocomplete.js

---
+++ JS/Autocomplete.js
@@ -0,0 +1,41 @@
+let Akeywords = [
+    'guitar',
+    'guitar classic',
+    'guitar accoustic',
+    'Drum',
+    'Accessory',
+    'guitar string',
+    'string',
+    'strings',
+    'guitar string',
+    'stands',
+    'cable',
+    'connector',
+];
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result= [];
+    let input = inputBox.value;
+    if(input.length){
+        result= Akeywords.filter((keyword)=>{
+          return  keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+    if(!result.length){
+        resultsBox.innerHTML= '';
+    }
+}
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick= selectInput(this)>"+ list+ "</li>";
+    });
+    resultsBox.innerHTML = "<ul>"+ content.join(' ') + "</ul>";
+}
+function selectInput(list){
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = '';
+}
